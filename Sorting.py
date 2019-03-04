0087fd7899786c249b3b9b60e8e05a952eab9f49
FabriceMinelli <fabrice.minelli.pro@gmail.com>
2019-03-04 10:18:20 +0100

    Add of the code which sort the raw files inside corresponding directory

---
+++ Sorting.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Folder creation
+path = os.path.dirname(os.path.abspath('__Tri.py__'))
+if not os.path.exists(path+'\images'):  #
+    os.mkdir(path+'\images')
+if not os.path.exists(path+'\Flat_field'):
+    os.mkdir(path+'\Flat_field')
+if not os.path.exists(path+'\Dark_frame'):
+    os.mkdir(path+'\Dark_frame')
+if not os.path.exists(path+'\Illumination'):
+    os.mkdir(path+'\Illumination')
+
+# Moving files into correct folder
+files=os.listdir('.')
+print(files)
+for file in files:
+    if "fit" in file:
+        if "FlatField" in file:
+            shutil.copyfile(file,path+'\Flat_field\ '+file)
+        elif "Dark" in file:
+            shutil.copyfile(file,path+'\Dark_frame\ '+file)
+        elif "illumination" in file:
+            shutil.copyfile(file, path + '\Illumination\ ' + file)
+        else:
+            shutil.copyfile(file, path + '\images\ ' + file)
+        os.remove(file)
